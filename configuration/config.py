68b354a7fd626121acd7fea791be97bb692d2f84
Alan Sakata <38868644+sakataalan@users.noreply.github.com>
2024-06-21 12:41:42 -0300

    Fix

--- configuration/config.py
+++ configuration/config.py
@@ -1,6 +1,6 @@
 class TestData:
     URL = "https://play.watch.tv.br/login"
-    USERNAME = "sakataalan@yahoo.com.br"
-    PASSWORD = "ytXX77R@R0ByT4C#"
+    USERNAME = ""
+    PASSWORD = ""
 
     IMPLICIT_WAIT = 10
